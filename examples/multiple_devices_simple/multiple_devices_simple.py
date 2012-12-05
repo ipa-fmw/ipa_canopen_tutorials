2649d6d987cbdedb715a710b4091a68614fe6312
ipa-tys <tys@ipa.fhg.de>
2012-12-05 18:12:57 +0100

    added examples

---
+++ examples/multiple_devices_simple/multiple_devices_simple.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import roslib
+roslib.load_manifest('ipa_canopen_tutorials')
+import rospy
+import copy
+from cob_srvs.srv import Trigger
+from brics_actuator.msg import JointVelocities
+from brics_actuator.msg import JointValue
+
+rospy.init_node("ipa_canopen_test")
+
+
+rospy.wait_for_service('/tray/init')
+initService = rospy.ServiceProxy('/tray/init', Trigger)
+resp = initService()
+print resp
+
+velPublisher = rospy.Publisher('/tray/command_vel', JointVelocities)
+rospy.sleep(2.0)
+v = JointVelocities()
+vv1 = JointValue()
+vv1.timeStamp = rospy.Time.now()
+vv1.joint_uri = "tray_1_joint"
+vv2 = copy.deepcopy(vv1)
+vv2.joint_uri = "tray_2_joint"
+vv3 = copy.deepcopy(vv1)
+vv3.joint_uri = "tray_3_joint"
+v.velocities = [vv1,vv2,vv3]
+
+v.velocities[0].value = 0.0
+
+while not rospy.is_shutdown():
+    v.velocities[0].value = 0.05
+    v.velocities[1].value = 0.05
+    velPublisher.publish(v)
+
+    rospy.sleep(1.0)
+
+    v.velocities[0].value = 0
+    v.velocities[1].value = 0
+    velPublisher.publish(v)
+
+    rospy.sleep(1.0)
+
+    v.velocities[0].value = - 0.05
+    v.velocities[1].value = - 0.05
+    velPublisher.publish(v)
+
+    rospy.sleep(1.0)
+
+    v.velocities[0].value = 0
+    v.velocities[1].value = 0
+    velPublisher.publish(v)
+
+    rospy.sleep(1.0)
